d403bd6a372317778ed63f8dbd1ac534c724539c
Nayan Dey <deyn4420@gmail.com>
2026-02-24 11:37:50 +0530

    Day 3: categorized files by type

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 namespace fs = std::filesystem;
 
 int main() {
-    std::cout << "Smart File Organizer - Day 1\n";
-    std::cout << "Listing all items in current directory:\n\n";
+    std::cout << "Smart File Organizer - Day 2\n";
+    std::cout << "Listing only files with extensions:\n\n";
 
     for (const auto& entry : fs::directory_iterator(".")) {
-        std::cout << entry.path() << std::endl;
+        if (entry.is_regular_file()) {
+            std::cout 
+                << entry.path().filename()
+                << " -> "
+                << entry.path().extension()
+                << std::endl;
+        }
     }
 
     return 0;
