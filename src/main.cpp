0d5ab3b47b60bfd2a284ba7520bd021b2390692e
Nayan Dey <deyn4420@gmail.com>
2026-02-25 22:49:33 +0530

    Day 4: Auto-create category folders

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
 #include <filesystem>
+#include <string>
 
 namespace fs = std::filesystem;
 
+std::string getCategory(const std::string& ext) {
+    if (ext == ".jpg" || ext == ".png" || ext == ".jpeg")
+        return "Images";
+    else if (ext == ".pdf" || ext == ".docx" || ext == ".txt" || ext == ".md")
+        return "Documents";
+    else if (ext == ".mp4" || ext == ".mkv")
+        return "Videos";
+    else if (ext == ".exe")
+        return "Executables";
+    else
+        return "Others";
+}
+
 int main() {
-    std::cout << "Smart File Organizer - Day 2\n";
-    std::cout << "Listing only files with extensions:\n\n";
+    std::cout << "Smart File Organizer - Day 4\n\n";
 
     for (const auto& entry : fs::directory_iterator(".")) {
         if (entry.is_regular_file()) {
-            std::cout 
-                << entry.path().filename()
-                << " -> "
-                << entry.path().extension()
-                << std::endl;
+            std::string ext = entry.path().extension().string();
+            std::string category = getCategory(ext);
+
+            if (!fs::exists(category)) {
+                fs::create_directory(category);
+                std::cout << "Created folder: " << category << std::endl;
+            }
         }
     }
 
