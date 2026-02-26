29c9ebbb946c10d7f6c18b62048aec688e1652d6
Nayan Dey <deyn4420@gmail.com>
2026-02-26 22:16:01 +0530

    Implemented file moving into category folders

--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,48 @@ std::string getCategory(const std::string& ext) {
         return "Others";
 }
 
+bool shouldSkip(const fs::path& file) {
+    std::string name = file.filename().string();
+
+    // Skip program itself and important project files
+    if (name == "organizer.exe" ||
+        name == ".gitignore" ||
+        name == "README.md")
+        return true;
+
+    return false;
+}
+
 int main() {
-    std::cout << "Smart File Organizer - Day 4\n\n";
+    std::cout << "Smart File Organizer - Day 5\n\n";
 
     for (const auto& entry : fs::directory_iterator(".")) {
         if (entry.is_regular_file()) {
+
+            if (shouldSkip(entry.path()))
+                continue;
+
             std::string ext = entry.path().extension().string();
             std::string category = getCategory(ext);
 
+            // Create folder if not exists
             if (!fs::exists(category)) {
                 fs::create_directory(category);
-                std::cout << "Created folder: " << category << std::endl;
+            }
+
+            fs::path destination = category / entry.path().filename();
+
+            // Move only if file not already there
+            if (!fs::exists(destination)) {
+                fs::rename(entry.path(), destination);
+                std::cout << "Moved: "
+                          << entry.path().filename()
+                          << " -> "
+                          << category << std::endl;
             }
         }
     }
 
+    std::cout << "\nOrganization Complete.\n";
     return 0;
 }
